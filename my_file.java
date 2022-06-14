e72d4f2d7e9e5c5730da653509a72316ce4c60ec
Abbas811 <103724537+Abbas811@users.noreply.github.com>
2022-06-14 13:58:53 +0530

    Update my_file.java

--- my_file.java
+++ my_file.java
@@ -2,5 +2,6 @@ class my_file{
     public static void main(String[] args) {
      System.out.println("Hello, World!");
      System.out.println("Hello, abbas"); 
+     System.out.println("Hellos");   
     }
 }
